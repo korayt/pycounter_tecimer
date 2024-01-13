1e8edc6aef9d23421a0325be77489e0d777db8d8
korayt <koray.tecimer@yahoo.com>
2024-01-12 21:26:27 -0800

    test deleted

--- tests/test_pycounter_tecimer.py
+++ tests/test_pycounter_tecimer.py
@@ -13,18 +13,6 @@ def test_count_words():
     actual = count_words("tests/einstein.txt")
     assert actual == expected, "Einstein quote counted incorrectly!"
 
-def test_plot_words():
-    """Test plotting of word counts."""
-    counts = Counter({'insanity': 1, 'is': 1, 'doing': 1,
-                      'the': 1, 'same': 1, 'thing': 1,
-                      'over': 2, 'and': 2, 'expecting': 1,
-                      'different': 1, 'results': 1})
-    fig = plot_words(counts)
-    assert isinstance(fig, matplotlib.container.BarContainer), \
-           "Wrong plot type"
-    assert len(fig.datavalues) == 10, \
-           "Incorrect number of bars plotted"
-
 
 def test_plot_words_error():
     """Check TypeError raised when Counter not used."""
