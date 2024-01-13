2f7e44a6b3d6a48300dc9fb74f2e85784def1adb
korayt <koray.tecimer@yahoo.com>
2024-01-12 21:27:53 -0800

    tests deleted

--- tests/test_pycounter_tecimer.py
+++ tests/test_pycounter_tecimer.py
@@ -1,8 +1,5 @@
 from pycounter_tecimer.pycounter_tecimer import count_words
-from pycounter_tecimer.plotting import plot_words
-import matplotlib
 from collections import Counter
-import pytest
 
 def test_count_words():
     """Test word counting from a file."""
@@ -13,19 +10,3 @@ def test_count_words():
     actual = count_words("tests/einstein.txt")
     assert actual == expected, "Einstein quote counted incorrectly!"
 
-
-def test_plot_words_error():
-    """Check TypeError raised when Counter not used."""
-    with pytest.raises(TypeError):
-        list_object = ["Pythons", "are", "non", "venomous"]
-        plot_words(list_object)
-
-def test_integration():
-    """Test count_words() and plot_words() workflow."""
-    counts = count_words("tests/einstein.txt")
-    fig = plot_words(counts)
-    assert isinstance(fig, matplotlib.container.BarContainer), \
-           "Wrong plot type"
-    assert len(fig.datavalues) == 10, \
-           "Incorrect number of bars plotted"
-    assert max(fig.datavalues) == 2, "Highest word count should be 2"
